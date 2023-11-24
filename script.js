c7d86a7ccdc108a7978e3ee6fab12dc8147e149b
Martinien-dot <martiniengaba123@gmail.com>
2023-11-24 21:09:12 +0100

    Waoh
    Nice

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
+
 function toggleMenu(){
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+document.addEventListener('DOMContentLoaded', function() {
+  var submitButton = document.getElementById('submitButton');
+  submitButton.addEventListener('click', submitForm);
+});
+
+function submitForm() {
+  document.getElementById('email').value = '';
+  document.getElementById('message').value = '';
+  alert("Message sent, but there is no database to save it");
 }
